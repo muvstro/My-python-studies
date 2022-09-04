eaa45ffffb2c0ece5a80f8aa0671b40453f419bd
muvstro <112473600+muvstro@users.noreply.github.com>
2022-09-04 13:59:59 +0300

    account

--- account-2.py
+++ account-2.py
@@ -1,19 +1,23 @@
-username=input("Enter username: ")
+username=input("Enter username")
+password=input("Enter password")
+retries=3
 while True:
-    password=input("Enter password: ")
     if len(password)<8:
         print("Password is too short")
     elif username in password:
-        print("Password contains username: ")
+        print("Password contains username")
     else:
         print("Password for user",username,"set")
-        break    
-password2=input("Enter a password again: ")
-while True:
+        break 
+password2=input("Enter a password again")
+while retries>0:
     if password2 == password:
         print("Correct")
-        break
     else:
+        retries=retries-1
+    
         print("Incorrect,try again")
-
         
+                
+
+
